88a914859dfab94b0be0efaf287ee8329bccc3bd
gustavoleyria <gustavoleyria@yahoo.com.ar>
2022-07-06 15:10:02 -0300

    Commit Model Users

--- src/models/Users.js
+++ src/models/Users.js
@@ -8,5 +8,56 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        last_name:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        mail:{
+            type: DataTypes.STRING,
+            validate:{
+                isEmail: true,
+            },
+            allowNull: false,
+            unique: true
+        },
+        password:{
+            type: DataTypes.STRING(12),
+            validate:{
+                isAlphanumeric: true,
+            },
+            allowNull: false
+        },
+        id_rol:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        address:{
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        cellphone:{
+            type: DataTypes.INTEGER,
+            validate:{
+                isNumeric: true, 
+            },
+            allowNull: false
+        },
+        location:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        department:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        zip_code:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        reset_password:{
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
     });
 };
