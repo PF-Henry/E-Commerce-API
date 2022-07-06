c6a61148ee5cf1c41c3f4f1cdbec93fa556372c1
gustavoleyria <gustavoleyria@yahoo.com.ar>
2022-07-06 14:36:01 -0300

    Commit Model Orders

--- src/models/Orders.js
+++ src/models/Orders.js
@@ -4,9 +4,30 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     // defino el modelo
     sequelize.define('orders', {
-        name: {
-            type: DataTypes.STRING,
+        sell_date: {
+            type: DataTypes.DATEONLY,
             allowNull: false,
+            default: DataTypes.NOW
         },
+        sell_time:{
+            type: DataTypes.DATE,
+            allowNull: false,
+            default: DataTypes.NOW
+        },
+        id_users:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        total_sell:{
+            type: DataTypes.FLOAT,
+            validate:{
+                min: 0
+            },
+            allowNull: false
+        },
+        state:{
+            type: DataTypes.ENUM('creada', 'procesando', 'cancelada', 'completa'),
+            allowNull: false
+        }
     });
 };
