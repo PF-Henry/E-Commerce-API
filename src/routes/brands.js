9f350105afd02fd65db697422292edbdd7c06128
Sneider <electronsgm@gmail.com>
2022-07-06 21:48:34 -0500

    Initial router setup

---
+++ src/routes/brands.js
@@ -0,0 +1,46 @@
+const { Router } = require('express');
+const router = Router();
+
+const { Brands } = requiere('../db.js');
+
+
+router.get('/', async(req, res, next) => {
+    try {
+        const brands = await Brands.findAll();
+        res.status(200).json(brands);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/', async(req, res, next) => {
+    try {
+        const brand = await Brands.create(req.body);
+        res.status(201).json(brand);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.put('/:id', async(req, res, next) => {
+    try {
+        const brand = await Brands.findByPk(req.params.id);
+        await brand.update(req.body);
+        res.status(200).json(brand);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.delete('/:id', async(req, res, next) => {
+    try {
+        const brand = await Brands.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(brand);
+    } catch (error) {
+        next(error);
+    }
+})
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
+const routerBrand = require('./brands');
 
 
 const router = Router();
 
 // Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+router.use('/brands', routerBrand);
 
 
 module.exports = router;
