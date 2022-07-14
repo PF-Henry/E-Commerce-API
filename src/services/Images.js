30c6c750c1351eec19a8dfd7bdb7ba057c978fcf
gerardograziano <cggraziano@gmail.com>
2022-07-14 00:03:49 -0300

    Model add Roles and Permissions

---
+++ src/services/Images.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const { Images } = require("../db.js");
+
+class serviceImages {
+    constructor() {
+        this.images = [];
+    }
+
+    async getAll() {
+        try {
+            return await Images.findAll({
+            });
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async getById(id) {
+        return await Images.findByPk(id);
+    }
+
+
+    async create(image) {
+        const { url_image } = image;
+        try {
+            if (!url_image) {
+                throw 'URL image is requerid field.';
+            }
+
+            const regCategory = { url_image };
+
+            // const newCategory = await Images.create(regCategory);
+
+            return { msg: 'The image was created successfully' };
+
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async update(id, image) {
+        try {
+            return await Images.update(image, {
+                where: {
+                    id: id
+                }
+            });
+        } catch (error) {
+            return error;
+        }
+    }
+    
+    
+    async delete(id) {
+        try {
+            return await Images.destroy({
+                where: {
+                    id: id
+                }
+            });
+        } catch (error) {
+            return error;
+        }
+    }
+}
+
+module.exports = serviceImages;
