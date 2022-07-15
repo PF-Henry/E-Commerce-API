e95f20f7922955de13081b22e3f02f2b125baa5d
gustavoleyria <gustavoleyria@yahoo.com.ar>
2022-07-15 01:40:10 -0300

    Routes and Services Permissions

--- src/routes/permissions.js
+++ src/routes/permissions.js
@@ -1,80 +1,67 @@
 const { Router } = require('express');
 const router = Router();
-const serviceProducts = require('../services/Permissions.js');
-const service = new serviceProducts();
+const servicePermissions = require('../services/Permissions.js');
+const service = new servicePermissions();
 const returnErrorMessage = require("../utils/msgErrors.js");
 // const { Products } = require('../db.js');
 
 
-router.get('/:rol', async(req, res, next) => {
-    const { rol } = req.params;
+router.get('/', async(req, res, next) => {
     try {
-        if(rol === "Admin" || rol === "Guest" || rol === "User"){
-            const response = await service.getAll(rol);
-            return res.status(200).json(response);
-        }
-       else{
-        throw "Invalid Rol"
-    }        
+            const response = await service.getAll();
+            return res.status(200).json(response);    
     } catch (error) {
         return returnErrorMessage(error);
     }
 });
 
-// router.get('/:id', async(req, res, next) => {
-//     let { id } = req.params;
-//     try {
-//         const response = await service.getById(id);
-//         res.status(200).json(response);
-//     } catch (error) {
-//         next(error);
-//     }
-// });
+router.get('/:role', async(req, res, next) => {
+    const {role} = req.params;
+    try {
+            const response = await service.getByRol(role);
+            return res.status(200).json(response);    
+    } catch (error) {
+        return returnErrorMessage(error);
+    }
+});
 
-router.post('/rol', async(req, res, next) => {
+router.get('/:id', async(req, res, next) => {
+    let { id } = req.params;
+    try {
+        const response = await service.getById(id);
+        res.status(200).json(response);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/', async(req, res, next) => {
     let body = req.body;
     try {
-        if(rol === "Admin" || rol === "Guest" || rol === "User"){
             const response = await service.create(body);
             return res.status(201).json(response);
-        }
-        else{
-            throw "Invalid Rol"
-        }        
+              
     } catch (error) {
         return returnErrorMessage(error);
     }
 })
 
-router.put('/:rol', async(req, res, next) => {
-    let { rol } = req.params;
+router.put('/:id', async(req, res, next) => {
+    let { id } = req.params;
     let body = req.body;
     try {
-        if(rol === "Admin" || rol === "Guest" || rol === "User"){
-            const response = await service.update(rol, body);
-            res.status(200).json(response);
-        }
-        else{
-            throw "Invalid Rol"
-        }
-        
+            const response = await service.update(id, body);
+            res.status(200).json(response);        
     } catch (error) {
         return returnErrorMessage(error);
     }
 })
 
-router.delete('/:rol', async(req, res, next) => {
-    let { rol } = req.params;
-
+router.delete('/:id', async(req, res, next) => {
+    let { id } = req.params;
     try {
-        if(rol === "Admin" || rol === "Guest" || rol === "User"){
-            const response = await service.delete(rol);
-            res.status(200).json(response);
-        }
-        else{
-            throw "Invalid Rol"
-        }
-        
+            const response = await service.delete(id);
+            res.status(200).json(response);        
     } catch (error) {
         return returnErrorMessage(error);
     }
