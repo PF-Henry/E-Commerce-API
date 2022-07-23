15ce668861890ecf41b2716ecea9eca95fcbef85
Sneider <electronsgm@gmail.com>
2022-07-23 16:19:25 -0500

    Cookies setup with httpOnly=false

--- src/authentication/strategies/google.js
+++ src/authentication/strategies/google.js
@@ -6,21 +6,12 @@ const service = new serviceUser();
 const GOOGLE_CLIENT_ID = "559232330287-ctdb2lf5f65n3mmiu1pas5gie6oa3ljo.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "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";
 
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-    user = service.getById(user.id);
-    done(null, user);
-});
-
 //* Google Strategy for Signing Up
 const googleStrategySignUp = new Strategy({
         clientID: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
-        // callbackURL: 'https://hexatech-api.herokuapp.com/api/auth/google/callback',
-        callbackURL: "http://localhost:3001/api/auth/google/callback",
+        callbackURL: 'https://hexatech-api.herokuapp.com/api/auth/google/callback',
+        // callbackURL: "http://localhost:3001/api/auth/google/callback",
         passReqToCallback: true
     },
     async function(req, accessToken, refreshToken, profile, done) {
@@ -49,19 +40,27 @@ const googleStrategySignUp = new Strategy({
 const googleStrategySignIn = new Strategy({
         clientID: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3001/api/auth/google/signin",
-        // callbackURL: "https://hexatech-api.herokuapp.com/api/auth/google/signin",
+        // callbackURL: "http://localhost:3001/api/auth/google/signin",
+        callbackURL: "https://hexatech-api.herokuapp.com/api/auth/google/signin",
         passReqToCallback: true
     },
     async function(req, accessToken, refreshToken, profile, done) {
         const userByEmail = await service.getByEmail(profile.emails[0].value);
-        console.log(userByEmail);
         if (!userByEmail) {
             done(null, false);
         } else {
             done(null, userByEmail);
         }
     });
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    user = service.getById(user.id);
+    done(null, user);
+});
 
 module.exports = {
     googleStrategySignUp,
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const { request } = require("express");
+const { session } = require("passport");
 const passport = require("passport");
 
-// const CLIENT_URL = "https://hexatech-store.netlify.app";
-// const API_URL = "https://hexatech-api.herokuapp.com";
+const CLIENT_URL = "https://hexatech-store.netlify.app";
+const API_URL = "https://hexatech-api.herokuapp.com";
 
-const CLIENT_URL = "http://localhost:3000";
-const API_URL = "http://localhost:3001";
+// const CLIENT_URL = "http://localhost:3000";
+// const API_URL = "http://localhost:3001";
 const { signToken, verifyToken } = require("../utils/jwt");
 
 
@@ -58,18 +59,29 @@ router.get("/google/signin",
             console.log('user', req.user);
             const token = signToken(req.user);
             // res.append('Origin-Cookie', `token=${token}`);
+
             // res.append('Set-Cookie', `token=${token}`);
-            res.cookie('token', token, { sameSite: 'none', secure: true });
+            res.cookie('token', token, { sameSite: 'none', secure: true, httpOnly: false });
             res.redirect(CLIENT_URL);
+            // res.redirect(`${API_URL}/api/auth/login/success`);
+            // next(req.user);
         }
     }
 );
+
+// router.get("/login/success", (req, res) => {
+//     let token = req.cookies;
+
+//     console.log('cookie', token);
+//     console.log('user', req.user);
+//     res.json({ token: token });
+// });
 
 router.get("/register/failed", (req, res) => {
     let message = 'El usuario ya existe - Ahora inicia session';
     // res.append('Origin-Cookie', `error=${message}`);
     // res.append('Set-Cookie', `error=${message}`);
-    res.cookie('error', message, { sameSite: 'none', secure: true });
+    res.cookie('error', message, { sameSite: 'none', secure: true, httpOnly: false });
     res.redirect(`${CLIENT_URL}/login`);
 });
 
@@ -77,9 +89,12 @@ router.get("/login/failed", (req, res) => {
     let message = 'Acceso denegado - Debes registrarte';
     // res.append('Origin-Cookie', `error=${message}`);
     // res.append('Set-Cookie', `error=${message}`);
-    res.cookie('error', message, { sameSite: 'none', secure: true });
+    res.cookie('error', message, { sameSite: 'none', secure: true, httpOnly: false });
     res.redirect(`${CLIENT_URL}/register`);
 });
+
+
+
 
 
 module.exports = router;
