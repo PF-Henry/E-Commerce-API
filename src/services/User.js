febf00a3c5a0224c6727b300be4955bd6738aec0
gerardograziano <cggraziano@gmail.com>
2022-07-31 21:38:27 -0300

    Routes and service User update profiles reset password and soft Delete

--- src/services/User.js
+++ src/services/User.js
@@ -108,24 +108,128 @@ class serviceUsers {
         }
     }
 
-    async update(id, user) {
+
+    async updateProfileAdmin(id, user) {
+        const password = user.password;
+
         try {
-            const user = await Users.update(user, {
-                where: {
-                    id: id
-                }
-            });
-            if (user === 0) {
+            const hashedPassword = await hashPassword(password);
+
+            const userFound = await Users.findByPk(id);
+            if (!userFound) {
                 throw 'User not found';
             }
-            if (user === 1) {
-                return { msg: 'User updated successfully' };
-            }
-
+            
+            userFound.first_name = user.first_name;
+            userFound.last_name = user.last_name;
+            userFound.email = user.email;
+            userFound.cellphone = user.cellphone;
+            if (password.length) user.password = hashedPassword;
+            await userFound.save();
+            
+            return { msg: 'User updated successfully' };
         } catch (error) {
+            console.log(error)
             return returnErrorMessage(error);
         }
     }
+
+
+    async updateProfileUser(id, user) {
+        const password = user.password;
+
+        try {
+            const hashedPassword = await hashPassword(password);
+
+            const userFound = await Users.findByPk(id);
+            if (!userFound) {
+                throw 'User not found';
+            }
+        
+            userFound.first_name = user.first_name;
+            userFound.last_name = user.last_name;
+            userFound.email = user.email;
+            userFound.address = user.addres;
+            userFound.cellphone = user.cellphone;
+            userFound.location = user.location;
+            userFound.department = user.department;
+            userFound.zip_code = user.zip_code;           
+            if (password.length) user.password = hashedPassword;
+            await userFound.save();
+            //await userFound.update(user);
+            
+            return { msg: 'User updated successfully' };
+        } catch (error) {
+            console.log(error)
+            return returnErrorMessage(error);
+        }
+    }
+
+
+    async softDelete(userId, state) {
+        try {
+            const userFound = await Users.findByPk(userId);
+            if (!userFound) {
+                throw 'User not found';
+            }
+        
+            userFound.state = state;           
+            await userFound.save();
+            
+            return { msg: 'User updated successfully' };
+        } catch (error) {
+            console.log(error)
+            return returnErrorMessage(error);
+        }
+    }
+
+
+    async changeRole(userId, role) {
+        try {
+            const userFound = await Users.findByPk(userId);
+            if (!userFound) {
+                throw 'User not found';
+            }
+
+            // roles
+            const roleFound = await Roles.findOne({
+                where: { name: role }
+            });
+
+            if (!roleFound) {
+                throw 'Role not found';
+            }
+    
+            userFound.roleId = roleFound.id;           
+            await userFound.save();
+            
+            return { msg: 'User updated successfully' };
+        } catch (error) {
+            console.log(error)
+            return returnErrorMessage(error);
+        }
+    }
+    
+
+    async resetPassword(userId, state) {
+        try {
+            const userFound = await Users.findByPk(userId);
+            if (!userFound) {
+                throw 'User not found';
+            }
+        
+            userFound.reset_password = state;           
+            await userFound.save();
+            
+            return { msg: 'User updated successfully' };
+        } catch (error) {
+            console.log(error)
+            return returnErrorMessage(error);
+        }
+    }
+
+
+
 
     async delete(id) {
         try {
